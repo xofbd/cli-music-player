e71052d6f9babbccfaf492a5ea3737cda8519a9b
Don Fox <dfox09@gmail.com>
2024-04-27 22:17:22 -0400

    Use -P (regex flag) for second search term

--- src/utils.sh
+++ src/utils.sh
@@ -6,14 +6,14 @@ find_songs() {
   pattern_song="$(parse_pattern "$1")"
   pattern_other="$(parse_pattern "$2")"
 
-  fdfind --type f --ignore-case --regex "$pattern_song" "$3" | grep --ignore-case "$pattern_other"
+  fdfind --type f --ignore-case --regex "$pattern_song" "$3" | grep -P --ignore-case "$pattern_other"
 }
 
 find_albums() {
   pattern_album=$(parse_pattern "$1")
   pattern_other=$(parse_pattern "$2")
 
-  fdfind --type d --ignore-case --regex "$pattern_album" "$3" | grep --ignore-case "$pattern_other"
+  fdfind --type d --ignore-case --regex "$pattern_album" "$3" | grep -P --ignore-case "$pattern_other"
 }
 
 message_empty() {
