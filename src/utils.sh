b44109ad52ce348641d3d5ad8aa05ced6eed3e2e
Don Fox <dfox09@gmail.com>
2024-06-03 16:02:58 -0400

    Refactor common code into one fine function

--- src/utils.sh
+++ src/utils.sh
@@ -2,32 +2,28 @@ parse_pattern() {
   sed 's/ /[\\s_-]/g' <<< "$1"
 }
 
-find_songs() {
-  local pattern_song="$(parse_pattern "$1")"
-  local pattern_other="$(parse_pattern "$2")"
-  local directory="$3"
+_find() {
+  local type="$1"
+  local pattern_target="$(parse_pattern "$2")"
+  local pattern_other="$(parse_pattern "$3")"
+  local directory="$4"
 
-  if [[ "$4" == all ]]; then
+  if [[ "$5" == all ]]; then
     local limit=""
   else
-    local limit="--max-results "$4""
+    local limit="--max-results "$5""
   fi
 
-  fdfind --type f --ignore-case $limit --regex "$pattern_song" "$directory" | grep -P --ignore-case "$pattern_other"
+  fdfind --type "$type" --ignore-case $limit --regex "$pattern_target" "$directory" \
+    | grep -P --ignore-case "$pattern_other"
 }
 
 find_albums() {
-  local pattern_album="$(parse_pattern "$1")"
-  local pattern_other="$(parse_pattern "$2")"
-  local directory="$3"
+  _find d "$@"
+}
 
-  if [[ "$4" == all ]]; then
-    local limit=""
-  else
-    local limit="--max-results "$4""
-  fi
-
-  fdfind --type d --ignore-case $limit --regex "$pattern_album" "$directory" | grep -P --ignore-case "$pattern_other"
+find_songs() {
+  _find f "$@"
 }
 
 message_empty() {
